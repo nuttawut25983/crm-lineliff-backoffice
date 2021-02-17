3232e582758f6ba1e78f853258ef14daad1b6585
patiphon <gankstel2@gmail.com>
2021-02-17 10:51:55 +0700

    reset navbar and sidebar and mock calendar

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -1,8 +1,6 @@
 <template>
   <nav
-    id="sidebar"
     class="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-no-wrap md:overflow-hidden shadow-xl bg-white flex flex-wrap items-center justify-between relative md:w-32 z-10 py-4"
-    :class="active ? '' : 'd-none'"
   >
     <div
       class="md:flex-col md:items-stretch md:min-h-full md:flex-no-wrap px-0 flex flex-wrap items-center justify-between w-full mx-auto"
@@ -21,6 +19,7 @@
       <!-- Collapse -->
       <div
         class="md:flex md:flex-col md:items-stretch md:opacity-100 md:relative md:mt-2 md:shadow-none shadow absolute top-0 left-0 right-0 z-40 overflow-y-auto overflow-x-hidden h-auto items-center flex-1 rounded"
+        v-bind:class="collapseShow"
       >
         <!-- Collapse header -->
         <div
@@ -33,7 +32,7 @@
             <div class="w-6/12 flex justify-end">
               <button
                 type="button"
-                class="cursor-pointer text-black opacity-50  px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent"
+                class="cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent"
                 v-on:click="toggleCollapseShow('hidden')"
               >
                 <i class="fas fa-times"></i>
@@ -163,7 +162,7 @@
 export default {
   data() {
     return {
-      active: true
+      collapseShow: "hidden",
     };
   },
   methods: {
@@ -171,14 +170,6 @@ export default {
       this.collapseShow = classes;
     },
   },
-  mounted() {
-    // console.log(this.$parent)
-    this.$parent.$on('toggleNav', () => {
-      console.log("received")
-      this.active = !this.active
-      
-      console.log(this.active)
-    })
-  },
+  components: {},
 };
 </script>
